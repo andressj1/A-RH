75de888e4631364223ee1a8b2342b356e447c1af
andressj1 <gv.andres@outlook.es>
2024-03-09 20:47:48 -0500

    Preprocesamiento de; bases unidas

--- a_funciones.py
+++ a_funciones.py
@@ -8,16 +8,7 @@ import joblib
 from sklearn.preprocessing import StandardScaler ## escalar variables 
 
 ####Este archivo contienen funciones utiles a utilizar en diferentes momentos del proyecto
-
-###########Esta función permite ejecutar un archivo  con extensión .sql que contenga varias consultas
-
-def ejecutar_sql (nombre_archivo, cur):
-  sql_file=open(nombre_archivo)
-  sql_as_string=sql_file.read()
-  sql_file.close
-  cur.executescript(sql_as_string)
   
- 
 
 def imputar_f (df,list_cat):  
         

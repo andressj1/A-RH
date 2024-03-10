efaa9293bbabdda94971d2eeccc4215b5d24d78d
andressj1 <gv.andres@outlook.es>
2024-03-10 09:59:10 -0500

    ..

--- a_funciones.py
+++ a_funciones.py
@@ -15,4 +15,7 @@ def plot_pie_chart(data, values_col, names_col):
     fig = px.pie(data, values=values_col, names=names_col)
     fig.show()
 
-    
+#####Grafico de barras; bivariado 
+def plot_bar_chart(df, x_col, y_col, color_col, barmode):
+    fig = px.bar(df, x=x_col, y=y_col, color=color_col, barmode=barmode)
+    fig.show()

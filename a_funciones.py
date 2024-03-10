8e03f30ebf1b9f92184ab0afefab321cc18aa136
andressj1 <gv.andres@outlook.es>
2024-03-10 13:22:24 -0500

    Agrego de funcion

--- a_funciones.py
+++ a_funciones.py
@@ -10,10 +10,23 @@ import plotly.express as px
 import matplotlib.pyplot as plt  # gráficos
 
 
-
+#### Grafico para numericas vs variable objetivo 
 def vovsnum(df, x_col, y_col):
     fig = px.box(df, x=x_col, y=y_col, 
                  title=f'Distribución de {y_col} por {x_col}',
                  points='outliers',
                  labels={x_col: x_col, y_col: y_col})
+    return fig
+
+##### Grafico para categoricas; encuestas
+
+def vovsenc(df, x_col, y_col, color_col, title):
+    # Calcular el conteo de valores
+    counts = df.groupby([x_col, color_col]).size().reset_index(name=y_col)
+    
+    # Crear el gráfico de barras
+    fig = px.bar(counts, x=x_col, y=y_col, color=color_col, 
+                 title=title,
+                 labels={'Category': 'Categoría', 'Count': 'Cantidad de Empleados', 'Attrition': 'Attrition'})
+    fig.update_xaxes(tickvals=[1, 2, 3, 4, 5, 6])
     return fig

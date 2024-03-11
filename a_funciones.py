af8fc9a5eac1f48c3bb9d647389687963109117b
andressj1 <gv.andres@outlook.es>
2024-03-10 21:25:22 -0500

    Selecccion de variables

--- a_funciones.py
+++ a_funciones.py
@@ -10,6 +10,7 @@ import plotly.express as px
 import matplotlib.pyplot as plt  # gráficos
 
 
+
 #### Grafico para numericas vs variable objetivo 
 def vovsnum(df, x_col, y_col):
     fig = px.box(df, x=x_col, y=y_col, 
@@ -47,3 +48,16 @@ def vovsstr(df, x_col, y_col, color_col):
                  orientation='h',
                  color_discrete_map={'rf': 'red', 'rg': 'green', 'rh': 'blue'})  # Cambiar los colores aquí
     return fig
+
+
+##### Seleccion de variables 
+def sel_variables(modelos, X, y, threshold):
+    var_names_ac = np.array([])
+    for modelo in modelos:
+        modelo.fit(X, y)
+        sel = SelectFromModel(modelo, threshold=threshold, prefit=True)
+        var_names = X.columns[sel.get_support()]
+        var_names_ac = np.append(var_names_ac, var_names)
+        var_names_ac = np.unique(var_names_ac)
+    
+    return var_names_ac
